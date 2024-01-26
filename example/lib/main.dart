7c0b3f39ca2062c2719007ff09541a876c148571
natsuk4ze <natsukaze.midori@gmail.com>
2024-01-26 20:37:56 +0900

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:compassx/compassx.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() => runApp(const App());
 
@@ -10,24 +11,34 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              StreamBuilder<CompassXEvent>(
-                  stream: CompassX.events,
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) return const Text('No data');
-                    final compass = snapshot.data!;
-                    return Column(
-                      children: [
-                        Text('Heading: ${compass.heading}'),
-                        Text('Accuracy: ${compass.accuracy}'),
-                        Text('Should calibrate: ${compass.shouldCalibrate}'),
-                      ],
-                    );
-                  }),
-            ],
+        body: DefaultTextStyle(
+          style: Theme.of(context).textTheme.headlineSmall!,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                StreamBuilder<CompassXEvent>(
+                    stream: CompassX.events,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) return const Text('No data');
+                      final compass = snapshot.data!;
+                      return Column(
+                        children: [
+                          Text('Heading: ${compass.heading}'),
+                          Text('Accuracy: ${compass.accuracy}'),
+                          Text('Should calibrate: ${compass.shouldCalibrate}'),
+                          Transform.rotate(
+                            angle: compass.heading * math.pi / 180,
+                            child: Icon(
+                              Icons.arrow_upward_rounded,
+                              size: MediaQuery.of(context).size.width - 80,
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+              ],
+            ),
           ),
         ),
       ),
