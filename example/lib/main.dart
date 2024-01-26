d53d5e59bff3d56cb58cdf3e4983a9db77c7af8c
natsuk4ze <natsukaze.midori@gmail.com>
2024-01-27 00:12:10 +0900

    Add permission handler to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:compassx/compassx.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(const App());
 
@@ -14,31 +18,34 @@ class App extends StatelessWidget {
         body: DefaultTextStyle(
           style: Theme.of(context).textTheme.headlineSmall!,
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                StreamBuilder<CompassXEvent>(
-                  stream: CompassX.events,
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) return const Text('No data');
-                    final compass = snapshot.data!;
-                    return Column(
-                      children: [
-                        Text('Heading: ${compass.heading}'),
-                        Text('Accuracy: ${compass.accuracy}'),
-                        Text('Should calibrate: ${compass.shouldCalibrate}'),
-                        Transform.rotate(
-                          angle: compass.heading * math.pi / 180,
-                          child: Icon(
-                            Icons.arrow_upward_rounded,
-                            size: MediaQuery.of(context).size.width - 80,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ],
+            child: StreamBuilder<CompassXEvent>(
+              stream: CompassX.events,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return const Text('No data');
+                final compass = snapshot.data!;
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Heading: ${compass.heading}'),
+                    Text('Accuracy: ${compass.accuracy}'),
+                    Text('Should calibrate: ${compass.shouldCalibrate}'),
+                    Transform.rotate(
+                      angle: compass.heading * math.pi / 180,
+                      child: Icon(
+                        Icons.arrow_upward_rounded,
+                        size: MediaQuery.of(context).size.width - 80,
+                      ),
+                    ),
+                    FilledButton(
+                      onPressed: () async {
+                        if (!Platform.isAndroid) return;
+                        await Permission.location.request();
+                      },
+                      child: const Text('Request permission'),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
