770b425cc33916a115fc554f804f14d9360104d7
Midori <natsukaze.midori@gmail.com>
2024-03-27 14:44:16 +0900

    Fix typo

--- lib/src/compassx.dart
+++ lib/src/compassx.dart
@@ -2,7 +2,7 @@ import 'package:compassx/src/compassx_event.dart';
 import 'package:compassx/src/compassx_platform.dart';
 import 'package:flutter/foundation.dart';
 
-/// The top-level class of comapssx package.
+/// The top-level class of compassx package.
 ///
 /// See the [wiki](https://github.com/natsuk4ze/compassx/wiki) for details.
 @immutable
